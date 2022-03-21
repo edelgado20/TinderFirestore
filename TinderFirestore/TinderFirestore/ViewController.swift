efcffecc6e4d098cc608748addfe17c82d1efbf9
Edgar Delgado <delga157@mail.chapman.edu>
2022-03-21 15:59:42 -0700

    First Commit

---
+++ TinderFirestore/TinderFirestore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinderFirestore
+//
+//  Created by Edgar Delgado on 3/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("First Commit")
+    }
+
+
+}
+
